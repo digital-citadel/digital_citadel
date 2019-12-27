272c98625d7fbec88584973336187e2ddc7f3f2b
Sandesh Athreya <sandeshathreya@Sandeshs-MacBook-Pro.local>
2019-12-27 21:00:54 +0100

    changed vs code settings and added routing

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Home from './Home.js'
-import NotFound from './NotFound'
-
+import Home from "./Home.js";
+import NotFound from "./NotFound";
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="*" component={NotFound} />
+        <Route exact path='/' component={Home} />
+        <Route path='*' component={NotFound} />
       </Switch>
     </BrowserRouter>
-        
   );
 }
 
 export default App;
-
