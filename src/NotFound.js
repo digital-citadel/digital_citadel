932ad6c799555265488798f912e52e9cc5da1080
Sandesh Athreya <sandeshathreya@Sandeshs-MacBook-Pro.local>
2019-12-27 18:20:54 +0100

    Initial commit

---
+++ src/NotFound.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+class NotFound extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  };
+    }
+    render() {
+        return (
+            <h1>Hello</h1>
+        );
+    }
+}
+
+export default NotFound;
