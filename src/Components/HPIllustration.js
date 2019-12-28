beac950ac0350390f9fd570f311b499aa08ffa6b
Sandesh Athreya <sandeshathreya@Sandeshs-MBP.fritz.box>
2019-12-28 14:18:25 +0100

    Homepage design

---
+++ src/Components/HPIllustration.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const HPIllustration = () => {
+  return (
+    <div className='col-md-6'>
+      <img
+        alt='Start-up'
+        style={{
+          width: "600px",
+          height: "420px",
+          marginTop: "10vh",
+          marginLeft: "4vw"
+        }}
+        src={require("../Assets/undraw_startup_life_2du2.svg")}
+      />
+    </div>
+  );
+};
+
+export default HPIllustration;
---
+++ src/Components/HPMouseComponent.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { MDBIcon } from "mdbreact";
+
+const HPMouseComponent = () => {
+  return (
+    <MDBIcon
+      style={{ position: "absolute", bottom: "2vh", color: "red" }}
+      icon='mouse'
+      size='2x'
+    />
+  );
+};
+
+export default HPMouseComponent;
