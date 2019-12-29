b12600f41659bdb1b9049b5ef6a437270569bc01
Sandesh Athreya <sandeshathreya@Sandeshs-MacBook-Pro.local>
2019-12-29 16:02:36 +0100

    Team changes done

---
+++ src/Components/TeamCard.js
@@ -0,0 +1,151 @@
+import React, { Component } from "react";
+import {
+  MDBCol,
+  MDBCard,
+  MDBCardUp,
+  MDBCardBody,
+  MDBAvatar,
+  MDBRotatingCard,
+  MDBIcon
+} from "mdbreact";
+
+class TeamCard extends Component {
+  state = {
+    flipped: false
+  };
+  constructor(props) {
+    super(props);
+    console.log(props);
+  }
+
+  handleFlipping = () => {
+    this.setState({ flipped: !this.state.flipped });
+  };
+
+  render() {
+    const colStyle = { maxWidth: "18rem" };
+
+    return (
+      // <MDBRow>
+      <MDBCol className='col-md-2' style={{ minHeight: "26rem" }}>
+        <MDBRotatingCard
+          flipped={this.state.flipped}
+          className='text-center h-100 w-100'
+          style={colStyle}
+        >
+          <MDBCard className='face front' style={{ background: "#0A1250" }}>
+            <MDBCardUp
+              style={{ height: "60px", background: "#0D0B34" }}
+            ></MDBCardUp>
+            <MDBAvatar className='mx-auto white' circle>
+              <img
+                src='https://mdbootstrap.com/img/Photos/Avatars/img%20(20).jpg'
+                alt=''
+                className='rounded-circle'
+              />
+            </MDBAvatar>
+            <MDBCardBody>
+              <h4
+                className='font-weight-bold mb-3'
+                style={{ color: "#e37373" }}
+              >
+                {" "}
+                {this.props.emp.name}{" "}
+              </h4>
+              <p
+                className='font-weight-bold white-text'
+                style={{ margin: "0" }}
+              >
+                {this.props.emp.role}
+              </p>
+              <p className='white-text'>{this.props.emp.oneLiner}</p>
+              <a
+                href='#!'
+                className='rotate-btn font-weight-bold'
+                data-card='card-1'
+                onClick={this.handleFlipping}
+              >
+                What do I do?
+              </a>
+              <ul className='list-inline py-2' style={{ marginTop: "16px" }}>
+                <li className='list-inline-item'>
+                  <a href='#!' className='p-2 fa-lg fb-ic'>
+                    <MDBIcon icon='facebook' brand style={{ color: "white" }} />
+                  </a>
+                </li>
+                <li className='list-inline-item'>
+                  <a href='#!' className='p-2 fa-lg tw-ic'>
+                    <MDBIcon icon='twitter' brand style={{ color: "white" }} />
+                  </a>
+                </li>
+                <li className='list-inline-item'>
+                  <a href='#!' className='p-2 fa-lg gplus-ic'>
+                    <MDBIcon
+                      icon='google-plus'
+                      brand
+                      style={{ color: "white" }}
+                    />
+                  </a>
+                </li>
+                <li className='list-inline-item'>
+                  <a href='#!' className='p-2 fa-lg li-ic'>
+                    <MDBIcon icon='linkedin' brand style={{ color: "white" }} />
+                  </a>
+                </li>
+              </ul>
+            </MDBCardBody>
+          </MDBCard>
+          <MDBCard className='face back' style={{ background: "#0A1250" }}>
+            <MDBCardBody>
+              <h4 className='font-weight-bold white-text'>About me</h4>
+              <hr />
+              <p className='white-text'>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime
+                quae, dolores dicta.
+              </p>
+              <hr />
+              <ul className='list-inline py-2' style={{ marginTop: "16px" }}>
+                <li className='list-inline-item'>
+                  <a href='#!' className='p-2 fa-lg fb-ic'>
+                    <MDBIcon icon='facebook' brand style={{ color: "white" }} />
+                  </a>
+                </li>
+                <li className='list-inline-item'>
+                  <a href='#!' className='p-2 fa-lg tw-ic'>
+                    <MDBIcon icon='twitter' brand style={{ color: "white" }} />
+                  </a>
+                </li>
+                <li className='list-inline-item'>
+                  <a href='#!' className='p-2 fa-lg gplus-ic'>
+                    <MDBIcon
+                      icon='google-plus'
+                      brand
+                      style={{ color: "white" }}
+                    />
+                  </a>
+                </li>
+                <li className='list-inline-item'>
+                  <a href='#!' className='p-2 fa-lg li-ic'>
+                    <MDBIcon icon='linkedin' brand style={{ color: "white" }} />
+                  </a>
+                </li>
+              </ul>
+              <a
+                href='#!'
+                className='rotate-btn'
+                data-card='card-1'
+                onClick={this.handleFlipping}
+              >
+                <MDBIcon icon='undo' /> Click here to rotate back
+              </a>
+            </MDBCardBody>
+          </MDBCard>
+        </MDBRotatingCard>
+      </MDBCol>
+
+      // </MDBRow>
+    );
+  }
+}
+
+export default TeamCard;
---
+++ src/data.js
@@ -0,0 +1,92 @@
+export default [
+  {
+    id: 1,
+    name: "Nithin",
+    role: "Devops Engineer",
+    oneLiner: "3+ years of experience",
+    description:
+      "Street art edison bulb gluten-free, tofu try-hard lumbersexual brooklyn tattooed pickled chambray. Actually humblebrag next level, deep v art party wolf tofu direct trade readymade sustainable hell of banjo. Organic authentic subway tile cliche palo santo, street art XOXO dreamCatcher retro sriracha portland air plant kitsch stumptown. Austin small batch squid gastropub. Pabst pug tumblr gochujang offal retro cloud bread bushwick semiotics before they sold out sartorial literally mlkshk. Vaporware hashtag vice, sartorial before they sold out pok pok health goth trust fund cray.",
+    skils: ["Devops", "Web design", "Cloud managment", "DB design"]
+  },
+  {
+    id: 2,
+    name: "Sandesh",
+    role: " Web Engineer",
+    oneLiner: "3+ years of experience",
+    description:
+      "Street art edison bulb gluten-free, tofu try-hard lumbersexual brooklyn tattooed pickled chambray. Actually humblebrag next level, deep v art party wolf tofu direct trade readymade sustainable hell of banjo. Organic authentic subway tile cliche palo santo, street art XOXO dreamCatcher retro sriracha portland air plant kitsch stumptown. Austin small batch squid gastropub. Pabst pug tumblr gochujang offal retro cloud bread bushwick semiotics before they sold out sartorial literally mlkshk. Vaporware hashtag vice, sartorial before they sold out pok pok health goth trust fund cray.",
+    skils: ["UI UX", "Web design", "Cloud managment", "DB design"]
+  },
+  {
+    id: 3,
+    name: "Akshay",
+    role: " UI/UX designer",
+    oneLiner: "3+ years of experience",
+    description:
+      "Street art edison bulb gluten-free, tofu try-hard lumbersexual brooklyn tattooed pickled chambray. Actually humblebrag next level, deep v art party wolf tofu direct trade readymade sustainable hell of banjo. Organic authentic subway tile cliche palo santo, street art XOXO dreamCatcher retro sriracha portland air plant kitsch stumptown. Austin small batch squid gastropub. Pabst pug tumblr gochujang offal retro cloud bread bushwick semiotics before they sold out sartorial literally mlkshk. Vaporware hashtag vice, sartorial before they sold out pok pok health goth trust fund cray.",
+    skils: ["UI UX", "Web design", "Cloud managment", "DB design"]
+  },
+  {
+    id: 4,
+    name: "Gautham",
+    role: "Frontend designer",
+    oneLiner: "3+ years of experience",
+    description:
+      "Street art edison bulb gluten-free, tofu try-hard lumbersexual brooklyn tattooed pickled chambray. Actually humblebrag next level, deep v art party wolf tofu direct trade readymade sustainable hell of banjo. Organic authentic subway tile cliche palo santo, street art XOXO dreamCatcher retro sriracha portland air plant kitsch stumptown. Austin small batch squid gastropub. Pabst pug tumblr gochujang offal retro cloud bread bushwick semiotics before they sold out sartorial literally mlkshk. Vaporware hashtag vice, sartorial before they sold out pok pok health goth trust fund cray.",
+    skils: ["Devops", "Web design", "Cloud managment", "DB design"]
+  },
+  {
+    id: 5,
+    name: "Shivani",
+    role: "UI/UX designer",
+    oneLiner: "3+ years of experience",
+    description:
+      "Street art edison bulb gluten-free, tofu try-hard lumbersexual brooklyn tattooed pickled chambray. Actually humblebrag next level, deep v art party wolf tofu direct trade readymade sustainable hell of banjo. Organic authentic subway tile cliche palo santo, street art XOXO dreamCatcher retro sriracha portland air plant kitsch stumptown. Austin small batch squid gastropub. Pabst pug tumblr gochujang offal retro cloud bread bushwick semiotics before they sold out sartorial literally mlkshk. Vaporware hashtag vice, sartorial before they sold out pok pok health goth trust fund cray.",
+    skils: ["UI UX", "Web design", "Cloud managment", "DB design"]
+  },
+  {
+    id: 1,
+    name: "Nithin",
+    role: "Devops Engineer",
+    oneLiner: "3+ years of experience",
+    description:
+      "Street art edison bulb gluten-free, tofu try-hard lumbersexual brooklyn tattooed pickled chambray. Actually humblebrag next level, deep v art party wolf tofu direct trade readymade sustainable hell of banjo. Organic authentic subway tile cliche palo santo, street art XOXO dreamCatcher retro sriracha portland air plant kitsch stumptown. Austin small batch squid gastropub. Pabst pug tumblr gochujang offal retro cloud bread bushwick semiotics before they sold out sartorial literally mlkshk. Vaporware hashtag vice, sartorial before they sold out pok pok health goth trust fund cray.",
+    skils: ["Devops", "Web design", "Cloud managment", "DB design"]
+  },
+  {
+    id: 2,
+    name: "Sandesh",
+    role: " Web Engineer",
+    oneLiner: "3+ years of experience",
+    description:
+      "Street art edison bulb gluten-free, tofu try-hard lumbersexual brooklyn tattooed pickled chambray. Actually humblebrag next level, deep v art party wolf tofu direct trade readymade sustainable hell of banjo. Organic authentic subway tile cliche palo santo, street art XOXO dreamCatcher retro sriracha portland air plant kitsch stumptown. Austin small batch squid gastropub. Pabst pug tumblr gochujang offal retro cloud bread bushwick semiotics before they sold out sartorial literally mlkshk. Vaporware hashtag vice, sartorial before they sold out pok pok health goth trust fund cray.",
+    skils: ["UI UX", "Web design", "Cloud managment", "DB design"]
+  },
+  {
+    id: 3,
+    name: "Akshay",
+    role: " UI/UX designer",
+    oneLiner: "3+ years of experience",
+    description:
+      "Street art edison bulb gluten-free, tofu try-hard lumbersexual brooklyn tattooed pickled chambray. Actually humblebrag next level, deep v art party wolf tofu direct trade readymade sustainable hell of banjo. Organic authentic subway tile cliche palo santo, street art XOXO dreamCatcher retro sriracha portland air plant kitsch stumptown. Austin small batch squid gastropub. Pabst pug tumblr gochujang offal retro cloud bread bushwick semiotics before they sold out sartorial literally mlkshk. Vaporware hashtag vice, sartorial before they sold out pok pok health goth trust fund cray.",
+    skils: ["UI UX", "Web design", "Cloud managment", "DB design"]
+  },
+  {
+    id: 4,
+    name: "Gautham",
+    role: "Frontend designer",
+    oneLiner: "3+ years of experience",
+    description:
+      "Street art edison bulb gluten-free, tofu try-hard lumbersexual brooklyn tattooed pickled chambray. Actually humblebrag next level, deep v art party wolf tofu direct trade readymade sustainable hell of banjo. Organic authentic subway tile cliche palo santo, street art XOXO dreamCatcher retro sriracha portland air plant kitsch stumptown. Austin small batch squid gastropub. Pabst pug tumblr gochujang offal retro cloud bread bushwick semiotics before they sold out sartorial literally mlkshk. Vaporware hashtag vice, sartorial before they sold out pok pok health goth trust fund cray.",
+    skils: ["Devops", "Web design", "Cloud managment", "DB design"]
+  },
+  {
+    id: 5,
+    name: "Shivani",
+    role: "UI/UX designer",
+    oneLiner: "3+ years of experience",
+    description:
+      "Street art edison bulb gluten-free, tofu try-hard lumbersexual brooklyn tattooed pickled chambray. Actually humblebrag next level, deep v art party wolf tofu direct trade readymade sustainable hell of banjo. Organic authentic subway tile cliche palo santo, street art XOXO dreamCatcher retro sriracha portland air plant kitsch stumptown. Austin small batch squid gastropub. Pabst pug tumblr gochujang offal retro cloud bread bushwick semiotics before they sold out sartorial literally mlkshk. Vaporware hashtag vice, sartorial before they sold out pok pok health goth trust fund cray.",
+    skils: ["UI UX", "Web design", "Cloud managment", "DB design"]
+  }
+];
