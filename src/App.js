f630e3c36d5bb3250f0a2eab9cdab0fdb44f3c5f
Sandesh Athreya <sandeshathreya@Sandeshs-MBP.fritz.box>
2019-12-29 12:08:28 +0100

    New changes in folder structure and css added for home section

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Home from "./Pages/Home.js";
 import NotFound from "./Components/NotFound";
+import SinglePage from "./Pages/SinglePage.js";
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path='/' component={Home} />
+        <Route exact path='/' component={SinglePage} />
         <Route path='*' component={NotFound} />
       </Switch>
     </BrowserRouter>
--- src/Sections/Home.js
+++ src/Sections/Home.js
@@ -3,18 +3,18 @@ import { MDBContainer } from "mdbreact";
 import Navbar from "../Components/Navbar";
 import HPTextComponent from "../Components/HPTextComponent";
 import HPIllustration from "../Components/HPIllustration";
-//import HPMouseComponent from "../Components/HPMouseComponent";
+import HPMouseComponent from "../Components/HPMouseComponent";
 
 export default class Home extends Component {
   render() {
     return (
       <MDBContainer fluid>
         <Navbar />
-        <div className='row col-md-12'>
+        <div className='row col-md-12' style={{ height: "100vh" }}>
           <HPTextComponent />
           <HPIllustration />
+          <HPMouseComponent />
         </div>
-        {/* <HPMouseComponent /> */}
       </MDBContainer>
     );
   }
