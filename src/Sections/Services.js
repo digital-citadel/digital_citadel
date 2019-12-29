777960cf77929d97f6d66b57172570d616b2c56f
akshaykumar771 <akshaykumar771@gmail.com>
2019-12-29 19:37:09 +0100

    Services

--- src/Sections/Services.js
+++ src/Sections/Services.js
@@ -2,12 +2,21 @@ import React, { Component } from 'react';
 import { MDBTypography, MDBContainer } from "mdbreact";
 import SPTextComponent from '../Components/SPTextComponent';
 import SPCardComponent from '../Components/SPCardComponent';
+import servicedata from "../servicedata";
 export default class Services extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
+    state = {
+        services: []
+      };
+      componentDidMount() {
+        let services = servicedata;
+        this.setState({
+          services
+        });
       }
     render() {
+        let ser = this.state.services.map(service => {
+            return <SPCardComponent service={service} />;
+          });
         return (
             <section>
         <MDBContainer fluid>
@@ -20,7 +29,7 @@ export default class Services extends Component {
             Services
           </MDBTypography>
           <SPTextComponent />
-          <SPCardComponent />
+          <div className='row col-md-12' style={{ display:"flex", justifyContent:"center", alignItems:"center", padding: "0 13%"}}>{ser}</div>
         </MDBContainer>
       </section>
         )
