8d98580fff7b2501390999fa0d42faf4adc790fa
Sandesh Athreya <sandeshathreya@Sandeshs-MBP.fritz.box>
2019-12-30 15:53:27 +0100

    Modal Added

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -30,16 +30,6 @@ class Navbar extends Component {
           style={{ marginTop: "20px" }}
         >
           <MDBNavbarBrand>
-            <img
-              alt='Start-up'
-              style={{
-                width: "60px",
-                height: "30px"
-                // marginTop: "10vh",
-                // marginLeft: "4vw"
-              }}
-              src={require("../Assets/DC.png")}
-            />
             <strong style={{ color: "#E37373", marginLeft: "" }}>
               Digital Citadel
             </strong>
