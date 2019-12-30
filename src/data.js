b7ab4cdef3545f622c8faa91cbb5ef64f6649002
Sandesh Athreya <sandeshathreya@Sandeshs-MBP.fritz.box>
2019-12-30 20:48:35 +0100

    Team information changes

--- src/data.js
+++ src/data.js
@@ -5,21 +5,27 @@ export default [
     role: "Product Designer",
     oneLiner: "3+ years of experience",
     description:
-      "Street art edison bulb gluten-free, tofu try-hard lumbersexual brooklyn tattooed pickled chambray. Actually humblebrag next level, deep v art party wolf tofu direct trade readymade sustainable hell of banjo. Organic authentic subway tile cliche palo santo, street art XOXO dreamCatcher retro sriracha portland air plant kitsch stumptown. Austin small batch squid gastropub. Pabst pug tumblr gochujang offal retro cloud bread bushwick semiotics before they sold out sartorial literally mlkshk. Vaporware hashtag vice, sartorial before they sold out pok pok health goth trust fund cray.",
+      "I am a passionate Product Designer (UI/UX) and also a web developer. I love designing for the Web and Mobile. User centric design is my niche. Having a master's degree in CS, you are never shot of a developer with me. ",
     skils: ["Front-End", "Web design", "App Design", "SEO"],
     img:
-      "https://raw.githubusercontent.com/digital-citadel/digital_citadel/master/src/Assets/SandeshAthreya.jpeg"
+      "https://raw.githubusercontent.com/digital-citadel/digital_citadel/master/src/Assets/SandeshAthreya.jpeg",
+    Github: "https://github.com/SandeshAthreyaBD",
+    LinkedIn: "https://linkedin.com/in/sandesh94",
+    Facebook: "https://www.facebook.com/sandeshathreyabd"
   },
   {
     id: 2,
-    name: "Nithin BS",
+    name: "Nithin Bharadwaj",
     role: "DevOps Engineer",
     oneLiner: "3+ years of experience",
     description:
-      "Street art edison bulb gluten-free, tofu try-hard lumbersexual brooklyn tattooed pickled chambray. Actually humblebrag next level, deep v art party wolf tofu direct trade readymade sustainable hell of banjo. Organic authentic subway tile cliche palo santo, street art XOXO dreamCatcher retro sriracha portland air plant kitsch stumptown. Austin small batch squid gastropub. Pabst pug tumblr gochujang offal retro cloud bread bushwick semiotics before they sold out sartorial literally mlkshk. Vaporware hashtag vice, sartorial before they sold out pok pok health goth trust fund cray.",
+      "Currently pursuing a master's degree in computer science, I specialize in cloud architectures and automation. Developement operations is my niche. I have excellent skills in automating tasks with Docker and Kubernetes. ",
     skils: ["Docker", "Kubernetes", "Hosting", "Security"],
     img:
-      "https://raw.githubusercontent.com/digital-citadel/digital_citadel/master/src/Assets/IMG-20191229-WA0007.jpg"
+      "https://raw.githubusercontent.com/digital-citadel/digital_citadel/master/src/Assets/IMG-20191229-WA0007.jpg",
+    Github: "https://github.com/Nithinbs18",
+    LinkedIn: "https://linkedin.com/in/nithinbhardwaj",
+    Facebook: "https://www.facebook.com/nithin.bhardwaj"
   },
   {
     id: 3,
@@ -27,43 +33,54 @@ export default [
     role: "Web Developer",
     oneLiner: "3+ years of experience",
     description:
-      "Street art edison bulb gluten-free, tofu try-hard lumbersexual brooklyn tattooed pickled chambray. Actually humblebrag next level, deep v art party wolf tofu direct trade readymade sustainable hell of banjo. Organic authentic subway tile cliche palo santo, street art XOXO dreamCatcher retro sriracha portland air plant kitsch stumptown. Austin small batch squid gastropub. Pabst pug tumblr gochujang offal retro cloud bread bushwick semiotics before they sold out sartorial literally mlkshk. Vaporware hashtag vice, sartorial before they sold out pok pok health goth trust fund cray.",
+      "I am a web engineer and a designer. Any challenge involving Javascript, I am game! I always have an eye for UX/UI Design as well. Currently doing my masters in Computer Science, I specialize with ReactJS and Databases. ",
     skils: ["UI/UX", "Web design", "Web Development", "Database"],
     img:
-      "https://raw.githubusercontent.com/digital-citadel/digital_citadel/master/src/Assets/akt_pr.png"
+      "https://raw.githubusercontent.com/digital-citadel/digital_citadel/master/src/Assets/akt_pr.png",
+    Github: "https://github.com/akshaykumar771",
+    LinkedIn: "https://linkedin.com/in/akshaykt",
+    Facebook: "https://www.facebook.com/akshay.tadapatri"
   },
   {
     id: 4,
-    name: "Gautham",
-    role: "Frontend designer",
+    name: "Gautam Rattihalli",
+    role: "Frontend developer",
     oneLiner: "5+ years of experience",
     description:
-      "Street art edison bulb gluten-free, tofu try-hard lumbersexual brooklyn tattooed pickled chambray. Actually humblebrag next level, deep v art party wolf tofu direct trade readymade sustainable hell of banjo. Organic authentic subway tile cliche palo santo, street art XOXO dreamCatcher retro sriracha portland air plant kitsch stumptown. Austin small batch squid gastropub. Pabst pug tumblr gochujang offal retro cloud bread bushwick semiotics before they sold out sartorial literally mlkshk. Vaporware hashtag vice, sartorial before they sold out pok pok health goth trust fund cray.",
+      "Hai! I am a front-end web developer. I basically bring a website to life with my magic CSS fingers. Also pursuing my masters in Computer Science, I speacilize in front-end web framkeworks and technologies.   ",
     skils: ["Front-End", "Web design", "Database Design", "Web Development"],
     img:
-      "https://raw.githubusercontent.com/digital-citadel/digital_citadel/master/src/Assets/Screenshot_20191229-180908.jpg"
+      "https://raw.githubusercontent.com/digital-citadel/digital_citadel/master/src/Assets/Screenshot_20191229-180908.jpg",
+    Github: "https://github.com/geazy2590",
+    LinkedIn: "https://linkedin.com/in/gautam-rattihalli-96261355",
+    Facebook: "https://www.facebook.com/gautam.rattihalli"
   },
   {
     id: 5,
-    name: "Shivani",
-    role: "UI/UX designer",
+    name: "Shivani Moray",
+    role: "Usability Engineer",
     oneLiner: "2+ years of experience",
     description:
-      "Street art edison bulb gluten-free, tofu try-hard lumbersexual brooklyn tattooed pickled chambray. Actually humblebrag next level, deep v art party wolf tofu direct trade readymade sustainable hell of banjo. Organic authentic subway tile cliche palo santo, street art XOXO dreamCatcher retro sriracha portland air plant kitsch stumptown. Austin small batch squid gastropub. Pabst pug tumblr gochujang offal retro cloud bread bushwick semiotics before they sold out sartorial literally mlkshk. Vaporware hashtag vice, sartorial before they sold out pok pok health goth trust fund cray.",
+      "I specialize in Visual and Graphic Design. Also pursuing M.Sc in Germany, I specialize in creating UI Components and Usability Engineering. UX, User Research and Javascript is my area of expertise. Can't wait to hear your amazing Ideas!  ",
     skils: ["UI/UX", "Adobe XD", "Illustrator", "InDesign"],
     img:
-      "https://raw.githubusercontent.com/digital-citadel/digital_citadel/master/src/Assets/shivani.jpeg"
+      "https://raw.githubusercontent.com/digital-citadel/digital_citadel/master/src/Assets/shivani.jpeg",
+    Github: "https://github.com/shivanimoray",
+    LinkedIn: "https://linkedin.com/in/shivani-moray",
+    Facebook: "https://www.facebook.com/shivani.moray"
   },
   {
     id: 6,
-    name: "Anglita",
-    role: "Computer Engineer",
+    name: "Anglita Alvares",
+    role: "Project and Operations",
     oneLiner: "6+ years of experience",
-    description:
-      "Street art edison bulb gluten-free, tofu try-hard lumbersexual brooklyn tattooed pickled chambray. Actually humblebrag next level, deep v art party wolf tofu direct trade readymade sustainable hell of banjo. Organic authentic subway tile cliche palo santo, street art XOXO dreamCatcher retro sriracha portland air plant kitsch stumptown. Austin small batch squid gastropub. Pabst pug tumblr gochujang offal retro cloud bread bushwick semiotics before they sold out sartorial literally mlkshk. Vaporware hashtag vice, sartorial before they sold out pok pok health goth trust fund cray.",
+    description: "",
     skils: ["Project Management", "Development", "Data Analysis", "DB design"],
     img:
-      "https://raw.githubusercontent.com/digital-citadel/digital_citadel/master/src/Assets/WhatsApp%20Image%202019-12-29%20at%204.25.15%20PM.jpeg"
+      "https://raw.githubusercontent.com/digital-citadel/digital_citadel/master/src/Assets/WhatsApp%20Image%202019-12-29%20at%204.25.15%20PM.jpeg",
+    Github: "https://github.com/anglita",
+    LinkedIn: "https://linkedin.com/in/anglita-avina-alvares-61966551",
+    Facebook: "https://www.facebook.com/anglita.alvares"
   },
   {
     id: 7,
@@ -74,7 +91,10 @@ export default [
       "Street art edison bulb gluten-free, tofu try-hard lumbersexual brooklyn tattooed pickled chambray. Actually humblebrag next level, deep v art party wolf tofu direct trade readymade sustainable hell of banjo. Organic authentic subway tile cliche palo santo, street art XOXO dreamCatcher retro sriracha portland air plant kitsch stumptown. Austin small batch squid gastropub. Pabst pug tumblr gochujang offal retro cloud bread bushwick semiotics before they sold out sartorial literally mlkshk. Vaporware hashtag vice, sartorial before they sold out pok pok health goth trust fund cray.",
     skils: ["UI/UX", "Axure RP", "Customer Management", "Adobe"],
     img:
-      "https://raw.githubusercontent.com/digital-citadel/digital_citadel/master/src/Assets/WhatsApp%20Image%202019-12-29%20at%204.29.56%20PM.jpeg"
+      "https://raw.githubusercontent.com/digital-citadel/digital_citadel/master/src/Assets/WhatsApp%20Image%202019-12-29%20at%204.29.56%20PM.jpeg",
+    Github: "https://github.com/omarsyd",
+    LinkedIn: "https://linkedin.com/in/omar-syed-bb68ba55",
+    Facebook: "https://www.facebook.com/farooq.omar.syed"
   },
   {
     id: 8,
@@ -90,7 +110,10 @@ export default [
       "DB Modelling"
     ],
     img:
-      "https://raw.githubusercontent.com/digital-citadel/digital_citadel/master/src/Assets/WhatsApp%20Image%202019-12-21%20at%207.45.42%20PM.jpeg"
+      "https://raw.githubusercontent.com/digital-citadel/digital_citadel/master/src/Assets/WhatsApp%20Image%202019-12-21%20at%207.45.42%20PM.jpeg",
+    Github: "https://github.com/Preethi-V-3",
+    LinkedIn: "https://linkedin.com/in/preethi-venkatesh-22079957",
+    Facebook: "https://facebook.com"
   },
   {
     id: 9,
@@ -106,7 +129,10 @@ export default [
       "DB Modelling"
     ],
     img:
-      "https://raw.githubusercontent.com/digital-citadel/digital_citadel/master/src/Assets/IMG_20191229_162531.jpeg"
+      "https://raw.githubusercontent.com/digital-citadel/digital_citadel/master/src/Assets/IMG_20191229_162531.jpeg",
+    Github: "https://github.com/ashupatil228",
+    LinkedIn: "https://linkedin.com/in/ashwini-patil-b64250a8",
+    Facebook: "https://www.facebook.com/ashwini.ashu.395"
   },
   {
     id: 10,
@@ -117,6 +143,23 @@ export default [
       "Street art edison bulb gluten-free, tofu try-hard lumbersexual brooklyn tattooed pickled chambray. Actually humblebrag next level, deep v art party wolf tofu direct trade readymade sustainable hell of banjo. Organic authentic subway tile cliche palo santo, street art XOXO dreamCatcher retro sriracha portland air plant kitsch stumptown. Austin small batch squid gastropub. Pabst pug tumblr gochujang offal retro cloud bread bushwick semiotics before they sold out sartorial literally mlkshk. Vaporware hashtag vice, sartorial before they sold out pok pok health goth trust fund cray.",
     skils: ["Front-End", "Back-End", "DevOps", "Cloud Management"],
     img:
-      "https://raw.githubusercontent.com/digital-citadel/digital_citadel/master/src/Assets/FB_IMG_1577639897138__01.jpg"
+      "https://raw.githubusercontent.com/digital-citadel/digital_citadel/master/src/Assets/FB_IMG_1577639897138__01.jpg",
+    Github: "https://github.com/Pavanct",
+    LinkedIn: "https://linkedin.com/in/pavan-chitradurga-thammanna",
+    Facebook: "https://www.facebook.com/pavan.ct.395"
+  },
+  {
+    id: 10,
+    name: "Ashwin Bharadwaj",
+    role: "Front End Developer",
+    oneLiner: "3+ years of experience",
+    description:
+      "Street art edison bulb gluten-free, tofu try-hard lumbersexual brooklyn tattooed pickled chambray. Actually humblebrag next level, deep v art party wolf tofu direct trade readymade sustainable hell of banjo. Organic authentic subway tile cliche palo santo, street art XOXO dreamCatcher retro sriracha portland air plant kitsch stumptown. Austin small batch squid gastropub. Pabst pug tumblr gochujang offal retro cloud bread bushwick semiotics before they sold out sartorial literally mlkshk. Vaporware hashtag vice, sartorial before they sold out pok pok health goth trust fund cray.",
+    skils: ["Front-End", "Back-End", "DevOps", "Cloud Management"],
+    img:
+      "https://raw.githubusercontent.com/digital-citadel/digital_citadel/master/src/Assets/FB_IMG_1577639897138__01.jpg",
+    Github: "https://github.com/Pavanct",
+    LinkedIn: "https://linkedin.com/in/pavan-chitradurga-thammanna",
+    Facebook: "https://www.facebook.com/pavan.ct.395"
   }
 ];
